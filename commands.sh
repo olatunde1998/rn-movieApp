e479f409fd89b6befe4ff5dcfb183409e217c0f9
geodevcode <geodevinstitute@gmail.com>
2025-02-26 18:40:15 +0100

    commands.sh updated

--- commands.sh
+++ commands.sh
@@ -22,3 +22,7 @@ eas build:configure
 #       }
 #     },
 eas build -p android --profile preview
+
+# Once you've made changes on a branch, you can share them with your team by publishing an update. 
+# This allows you to get feedback on your changes during review.
+eas update --auto
