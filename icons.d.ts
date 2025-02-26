da0ba368adaff01c93e1ea4e3aa5ba1acc2f5fbd
geodevcode <geodevinstitute@gmail.com>
2025-02-26 09:04:02 +0100

    icons and images types declared

---
+++ icons.d.ts
@@ -0,0 +1,26 @@
+declare module "./icons" {
+  const icons: {
+    google: string;
+    home: string;
+    search: string;
+    person: string;
+    bell: string;
+    filter: string;
+    star: string;
+    heart: string;
+    backArrow: string;
+    info: string;
+    language: string;
+    logout: string;
+    people: string;
+    wallet: string;
+    rightArrow: string;
+    send: string;
+    area: string;
+    chat: string;
+    phone: string;
+    location: string;
+    edit: string;
+  };
+  export default icons;
+}
---
+++ images.d.ts
@@ -0,0 +1,33 @@
+declare module "*.png" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.gif" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: any;
+  export default value;
+}
+
+declare module "./images" {
+  const images: {
+    avatar: string;
+    map: string;
+    noResult: string;
+  };
+  export default images;
+}
