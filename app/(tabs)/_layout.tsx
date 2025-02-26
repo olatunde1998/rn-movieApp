113aa1b2a627a4edbe901ebf29f260adfbdca05f
geodevcode <geodevinstitute@gmail.com>
2025-02-26 09:08:07 +0100

    Tabs layout created

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,87 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import icons from "@/constants/icons";
+import { StatusBar } from "expo-status-bar";
+
+const TabIcon = ({
+  focused,
+  icon,
+  title,
+}: {
+  focused: boolean;
+  icon: any;
+  title: string;
+}) => (
+  <View className="flex-1 mt-3 flex flex-col items-center">
+    <Image
+      source={icon}
+      tintColor={focused ? "#FFA001" : "#CDCDE0"}
+      resizeMode="contain"
+      className="size-6"
+    />
+    <Text
+      className={`${
+        focused
+          ? "text-[#FFA001] font-rubik-medium"
+          : "text-gray-100 font-rubik"
+      } text-xs w-full text-center mt-1`}
+    >
+      {title}
+    </Text>
+  </View>
+);
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            position: "absolute",
+            borderTopColor: "#232533",
+            borderTopWidth: 1,
+            minHeight: 84,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon icon={icons.home} focused={focused} title="Home" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="bookmark"
+          options={{
+            title: "Bookmark",
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon icon={icons.bookmark} focused={focused} title="Saved" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon icon={icons.profile} focused={focused} title="Profile" />
+            ),
+          }}
+        />
+      </Tabs>
+      <StatusBar backgroundColor="#161622" style="auto" />
+    </>
+  );
+};
+
+export default TabsLayout;
