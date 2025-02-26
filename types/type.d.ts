024ba1550fab31114aadbccea80468496f9cc866
geodevcode <geodevinstitute@gmail.com>
2025-02-26 12:36:07 +0100

    type declaration file acreated

---
+++ types/type.d.ts
@@ -0,0 +1,37 @@
+import { TextInputProps, TouchableOpacityProps } from "react-native";
+
+declare interface ButtonProps extends TouchableOpacityProps {
+  title: string;
+  bgVariant?: "primary" | "secondary" | "danger" | "outline" | "success";
+  textVariant?: "primary" | "default" | "secondary" | "danger" | "success";
+  IconLeft?: React.ComponentType<any>;
+  IconRight?: React.ComponentType<any>;
+  className?: string;
+}
+
+declare interface GoogleInputProps {
+  icon?: string;
+  initialLocation?: string;
+  containerStyle?: string;
+  textInputBackgroundColor?: string;
+  handlePress: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => void;
+}
+
+declare interface FormFieldProps extends TextInputProps {
+  title?: string;
+  value?: string;
+  placeholder?: string;
+  handleChangeText?: (text: string) => void;
+  otherStyles?: string;
+  secureTextEntry?: boolean;
+  inputStyle?: string;
+  className?: string;
+}
