fc0234de37988d082f15e66f811846209185082e
geodevcode <geodevinstitute@gmail.com>
2025-02-26 12:36:32 +0100

    AuthLayout created

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,30 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function AuthLayout() {
+  return (
+    <>
+      <Stack>
+        <Stack.Screen
+          name="sign-in"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="sign-up"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="forgot-password"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
+  );
+}
