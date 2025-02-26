85102c0127da7192d93595bad77021bea5ed0270
geodevcode <geodevinstitute@gmail.com>
2025-02-26 09:01:25 +0100

    constants created for icons and images

---
+++ constants/images.ts
@@ -0,0 +1,9 @@
+// import brandLogo from "@/assets/images/logo.png";
+import noResult from "@/assets/images/no-result.png";
+import trash from "@/assets/images/empty.png";
+
+export default {
+  // brandLogo,
+  noResult,
+  trash,
+};
